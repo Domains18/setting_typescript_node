28923431edaf8109ff0cae55251d0f701a0ba496
Domains18 <gibsonsgibson88@gmail.com>
2023-02-18 23:44:26 +0300

    tailwind css

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div >hello</div>
+  )
+}
+
+export default Navbar
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import Navbar from '../components/Navbar'
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <div>
+      <Navbar />
+      <h1>Homepage</h1>
+    </div>
   )
 }
 
