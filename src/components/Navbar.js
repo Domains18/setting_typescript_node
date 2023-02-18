fdb254b69439e46a1a63ba9eadf30d677f776253
Domains18 <gibsonsgibson88@gmail.com>
2023-02-19 01:53:29 +0300

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,23 @@ const Navbar = () => {
       <div className='header-nav'>
         <div className='header-nav-logo'>
           <h1>Lelan BookStore</h1>
+          <div className="filter">
+            <div className="dropdown">
+              <span>Filter</span>
+              <div className="dropdown-content">
+                <a href="#">All</a>
+                <a href="#">Action</a>
+                <a href="#">Adventure</a>
+                <a href="#">Comedy</a>
+                <a href="#">Drama</a>
+                <a href="#">Fantasy</a>
+                <a href="#">Horror</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="search-bar">
+          <input type="text" placeholder="Search" />
         </div>
         <div className='header-nav-menu'>
           <ul>
