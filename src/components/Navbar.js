3fe048ed8993f9bc8786079d7531ebbdd060ba45
Domains18 <gibsonsgibson88@gmail.com>
2023-02-19 01:35:56 +0300

    nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,26 +2,22 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='header-nav'>
-      <div className='header-nav-logo'>
-        <h1>Lelan BookStore</h1>
+    <>
+      <div className='header-nav'>
+        <div className='header-nav-logo'>
+          <h1>Lelan BookStore</h1>
+        </div>
+        <div className='header-nav-menu'>
+          <ul>
+            <li>Home</li>
+            <li>Services</li>
+            <li>Portfolio</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </div>
       </div>
-      
-      <div className='header-nav-menu'>
-        <ul>
-        <hr />
-          <li>Home</li>
-          <li>Services</li>
-          <li>Portfolio</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </div>
-      <div className="search-bar">
-        <input className='search' type="text" placeholder="Search" />
-      </div>
-      <hr />
-    </div>
+    </>
   )
 }
 
