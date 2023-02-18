c9caaeb49d746a447139b4eb282206686ed68be5
Domains18 <gibsonsgibson88@gmail.com>
2023-02-18 23:34:17 +0300

    tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ "./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx" ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
