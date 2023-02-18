e37a04afe1cf4bc64c62f36c28baf0fec13e2c60
Domains18 <gibsonsgibson88@gmail.com>
2023-02-18 23:28:14 +0300

    front end setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import React from 'react'
 import './App.css'
-
+import Homepage from './pages/Homepage'
 function App() {
 
   return (
     <div className="app">
-      cleanup
+      <Homepage />
     </div>
   )
 }
