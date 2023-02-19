0710c7f9c58ec041fe6e864fb8e09a8e60b3a8be
Domains18 <gibsonsgibson88@gmail.com>
2023-02-19 03:37:43 +0300

    server

--- server/index.js
+++ server/index.js
@@ -3,4 +3,17 @@ const path = require('path');
 const dotenv = require('dotenv').config();
 const colors = require('colors');
 const errorHandler = require('./middleware/errorHandler');
-const 
+const PORT = process.env.PORT || 5000;
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.use(errorHandler);
+
+app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`.yellow.bold));
