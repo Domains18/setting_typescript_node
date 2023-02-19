732636b9ac1b1240ebbc745cc541b52e47084467
Domains18 <gibsonsgibson88@gmail.com>
2023-02-19 03:28:31 +0300

    .env

---
+++ server/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv').config();
+const colors = require('colors');
+const errorHandler = require('./middleware/errorHandler');
+const 
