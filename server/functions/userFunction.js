f342e653b578f5ab1596c282c009c365204aebe5
Domains18 <gibsonsgibson88@gmail.com>
2023-02-19 15:16:40 +0300

    API tests

--- server/functions/userFunction.js
+++ server/functions/userFunction.js
@@ -1,15 +1,2 @@
 const User = require('../models/user/userSchema');
-
-const registerUser = expressAsyncHandler(async (req, res) => {
-    const { names, email, password } = req.body;
-    const Users = [];
-
-    if (!names || !email || !password) {
-        res.status(400).json({ message: 'All fields are required' });
-    }
-    try {
-        
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
-});
+const expressAsyncHandler = require('express-async-handler');
