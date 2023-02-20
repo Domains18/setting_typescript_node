62baf6ec7fb6623bd8890caa3a223740382611c6
Domains18 <gibsonsgibson88@gmail.com>
2023-02-20 11:26:16 +0300

    changes to nav ui

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,31 @@
 import React from 'react'
 
 const Navbar = () => {
-  return (
-      <>
-          
-      </>
-  )
+    return (
+        <>
+            <nav className='bg-slate-200 h-[200px] shadow-xl'>
+                <div className="container flex justify-between pl-2 pr-10 ">
+                    <div className="first-sec">
+                        <div className="logo">
+                            <h1>Lelan Bookshop</h1>
+                        </div>
+                    </div>
+                    <div className="next-sec">
+                        <div className="search">
+                            <input type="text" placeholder="Search" />
+                            <button>Search</button>
+                        </div>
+                    </div>
+                    <div className="third-sec">
+                        <div className="cart">
+                            <i className="fas fa-shopping-cart"></i>
+                            <span>0</span>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
 }
 
 export default Navbar
