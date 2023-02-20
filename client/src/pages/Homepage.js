9b30849d84a4f967794988f89659049417bf8b54
Domains18 <gibsonsgibson88@gmail.com>
2023-02-20 16:14:09 +0300

    main

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -6,7 +6,15 @@ const Homepage = () => {
       <Navbar />
       <hr />
       <div className="home ">
-         
+        <div className="dropdown">
+          <button className="dropbtn"> filter by</button>
+          <div className="dropdown-content">
+            <a href="#">Grade 1</a>
+            <a href="#">Grade 2</a>
+            <a href="#">Grade 3</a>
+            <a href="#">Grade 4</a>
+          </div>
+         </div>
       </div>
     </>
   )
