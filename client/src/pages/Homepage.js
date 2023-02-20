19073cf84563fd572556e8e6f70cf061ae9b523f
Domains18 <gibsonsgibson88@gmail.com>
2023-02-20 16:00:58 +0300

    frontend

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -2,10 +2,13 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 const Homepage = () => {
   return (
-    <div>
+    <>
       <Navbar />
-      <h1>FrontEnd sucks</h1>
-    </div>
+      <hr />
+      <div className="home ">
+         
+      </div>
+    </>
   )
 }
 
