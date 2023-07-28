7709d2ba9f348bf6523d1f9eb8e5567a8038050d
Gibsons Gibson <gibsonsgibson88@gmail.com>
2023-07-28 14:49:42 +0300

    user

--- src/database/users.ts
+++ src/database/users.ts
@@ -1,4 +1,29 @@
 import mongoose from "mongoose";
 
 
-const useSchema = new mongoose.Schema({})
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: [true, 'please provide username'] },
+    email: { type: String, required: [true, 'Email is required'] },
+    authentication: {
+        password: { required: [true, 'please provide password'], type: String, select: false },
+        salt: { type: String, select: false },
+        sessionToken: { type: String, select: false }
+    }
+});
+
+
+export const userModel = mongoose.model('user', userSchema);
+
+export const getUsers = () => userModel.find();
+export const getUserByEmail =(email: string) => userModel.findOne({ email });
+export const getUserBySessionToken = ( sessionToken: string) => userModel.findOne({
+    'authentication.sessionToken': sessionToken,
+});
+export const getUserById = (id: string) => userModel.findById(id);
+
+
+export const createUser = (values: Record<string, any>) => new userModel(values).save()
+    .then((user)=> user.toObject());
+
+export const deleteUserByID = (id: string) => userModel.findOneAndDelete({_id : id});
+export updateUserById = (id: string, values: Record<string, any>) => userModel.
